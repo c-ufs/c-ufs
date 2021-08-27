897f395934e4455fc2e8be7c498fec50b508a193
itzkbp <itzkbp@gmail.com>
2021-08-27 12:53:32 +0545

    using String instead of pointer for strings

--- headers/cDataTypes.h
+++ headers/cDataTypes.h
@@ -1,2 +1,21 @@
 // String = Array of characters
 typedef char* string;
+
+//String = Structure of strlen & string
+typedef struct st_String { size_t contentLength ; char *content; } String;
+
+String setString(string str)
+{
+    String returnString;
+    int i = 0;
+    while(str[i]!='\0')
+    {
+        ;
+    }
+    return returnString;
+}
+
+String addCharacter(char ch)
+{
+
+}
