bd110195bb4eb548ad15ed4b6041810bdb46b76c
itzkbp <itzkbp@gmail.com>
2021-08-27 12:53:00 +0545

    added DB_STATUS for connection_string

--- dbms/typedefs.h
+++ dbms/typedefs.h
@@ -3,6 +3,6 @@
     #define INCLUDEDSTDIO_H
 #endif
 
-typedef struct st_Database{ FILE *fileDb; __uint128_t lastpk; char name[25]; } Database;
+typedef struct st_Database{ FILE *fileDb; __uint128_t lastpk; char name[25]; int DB_STATUS; } Database;
 
 typedef struct st_Table{ FILE *fileTbl; __uint128_t pk; } Table;
