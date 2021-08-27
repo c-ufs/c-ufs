249d0d1a5764f4748a60afa79739238f6a459260
itzkbp <itzkbp@gmail.com>
2021-08-27 13:56:36 +0545

    Srtring functions

--- headers/cDataTypes.h
+++ headers/cDataTypes.h
@@ -1,3 +1,8 @@
+#ifndef INCLUDEDSTRING_H
+    #include <string.h>
+    #define INCLUDEDSTRING_H
+#endif
+
 // String = Array of characters
 typedef char* string;
 
@@ -7,15 +12,22 @@ typedef struct st_String { size_t contentLength ; char *content; } String;
 String setString(string str)
 {
     String returnString;
-    int i = 0;
-    while(str[i]!='\0')
-    {
-        ;
-    }
+    returnString.content = str;
+    returnString.contentLength = strlen(str);
     return returnString;
 }
 
+string getString(String *str)
+{
+    return (str->content);
+}
+
 String addCharacter(char ch)
+{
+    
+}
+
+String addString(string str)
 {
 
 }
