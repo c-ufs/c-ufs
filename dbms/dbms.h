8c7b97ad0701b0e6e6f7586965b1492d779d6d30
itzkbp <itzkbp@gmail.com>
2021-08-27 09:40:59 +0545

    included stdlib.h

--- dbms/dbms.h
+++ dbms/dbms.h
@@ -1,18 +1,24 @@
 #ifndef INCLUDEDcDataTypes
-#include "..//headers//cDataTypes.h"
-#define INCLUDEDcDataTypes
+    #include "..//headers//cDataTypes.h"
+    #define INCLUDEDcDataTypes
 #endif
 
-//Primary Data Files
+// #region -> Primary Data Files
 #ifndef INCLUDEDSTDIO_H
-#include <stdio.h>
-#define INCLUDEDSTDIO_H
+    #include <stdio.h>
+    #define INCLUDEDSTDIO_H
 #endif
 
 #ifndef INCLUDEDSTRING_H
-#include <string.h>
-#define INCLUDEDSTRING_H
+    #include <string.h>
+    #define INCLUDEDSTRING_H
 #endif
+
+#ifndef INCLUDEDSTDLIB_H
+    #include <stdlib.h>
+    #define INCLUDEDSTDLIB_H
+#endif
+// #endregion -> Primary Data Files
 
 typedef struct st_Database
 {
@@ -48,6 +54,16 @@ Table openTable(string tableName)
 
     Table table; //= table_constructor(fileTbl,...);
     return table;
+}
+
+FILE *tryOpening(string fileName, string fileMode)
+{
+    FILE *fp = fopen(fileName, fileMode);
+
+    if(fp == NULL)
+        perror("Exiting....\nFILE error :"), exit(1);
+
+    return fp;
 }
 
 void closeTable(Table table)
